baa3b8ddeeb504606fbf24962bf3b51bd1b31059
user.name <omanovic.edo@gmail.com>
2023-04-08 12:52:42 +0200

    - bounded data to property list; some .css work done

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent {
-
+  properties: Array<any> = [
+    {
+    "Id": 1,
+    "Name": "Birla house",
+    "Type": "House",
+    "Price": 12000
+  },
+  {
+    "Id": 2,
+    "Name": "Erose house",
+    "Type": "House",
+    "Price": 23000
+  }, {
+    "Id": 3,
+    "Name": "Small house",
+    "Type": "House",
+    "Price": 7000
+  },
+  {
+    "Id": 4,
+    "Name": "Villa house",
+    "Type": "House",
+    "Price": 112000
+  },
+  {
+    "Id": 5,
+    "Name": "Dam house",
+    "Type": "House",
+    "Price": 55000
+  }
+]
 }
